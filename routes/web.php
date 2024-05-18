b33eed94704b1256989e2d34117459a8e9d9adfd
juin03 <juin5789@student.usm.my>
2024-05-18 10:55:07 +0800

    edited routes

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'welcome'] )->name('welcome');
 
-Route::get('/note', [NoteController::class,'index'])->name('note.index');
-Route::get('note/create', [NoteController::class,'create'])->name('note.create');
-Route::post('/note', [NoteController::class,'store'])->name('note.store');
-Route::get('note/{id}', [NoteController::class,'show'])->name('note.show');
-Route::get('note/{id}/edit', [NoteController::class,'edit'])->name('note.edit');
-Route::put('note/{id}', [NoteController::class,'update'])->name('note.update');
-Route::delete('note/{id}', [NoteController::class,'destroy'])->name('note.destroy');
+// Route::get('/note', [NoteController::class,'index'])->name('note.index');
+// Route::get('note/create', [NoteController::class,'create'])->name('note.create');
+// Route::post('/note', [NoteController::class,'store'])->name('note.store');
+// Route::get('note/{id}', [NoteController::class,'show'])->name('note.show');
+// Route::get('note/{id}/edit', [NoteController::class,'edit'])->name('note.edit');
+// Route::put('note/{id}', [NoteController::class,'update'])->name('note.update');
+// Route::delete('note/{id}', [NoteController::class,'destroy'])->name('note.destroy');
 
+//Basically this single row does what 7 rows above do
+Route::resource('note', NoteController::class);
